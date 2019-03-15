0b5db0a05b9024ad27ca2542c24c5a6fd929810e
Matheus Melo Antiquera <matheus.melo.a@hotmail.com>
2019-03-15 10:36:00 -0300

    Inicio do Projeto utilizando Vue

---
+++ resources/js/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">Blog Mind</a>
+    </nav>
+    <div class="container">
+      <div class="card">
+        <div class="card-header"><h2>Latest Posts</h2></div>
+        <div class="card-body">
+          <app-post v-for="p in posts" :post="p" :key="p.id"></app-post>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppPost from "./components/Post.vue";
+
+export default {
+  name: "app",
+  components: {
+    AppPost
+  },
+  data() {
+    return {
+      posts: []
+    };
+  },
+  methods: {
+    read() {
+      const posts = [
+        { id: "1", title: "Teste", text: "Testatando" },
+        { id: "2", title: "Teste", text: "Testatando" },
+        { id: "3", title: "Teste", text: "Testatando" }
+      ];
+
+      posts.forEach(post => {
+        this.posts.push(post);
+      });
+    }
+  },
+  mounted() {
+    this.read();
+  }
+};
+</script>
+
+<style>
+nav {
+  margin-bottom: 20px;
+}
+</style>
---
+++ resources/js/components/Post.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="post">
+    <div class="card mb-3">
+      <div class="row no-gutters">
+        <div class="col-md-4">
+          <img
+            src="https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg?auto=compress&cs=tinysrgb&dpr=1"
+            class="card-img"
+            alt
+          >
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">
+              Card title
+              <span class="buttons-right">
+                <a :href="edit()" role="button" class="btn btn-warning"><i class="fa fa-pencil-square-o fa-lg"></i>Edit</a>
+                <a href="#" role="button" class="btn btn-danger"><i class="fa fa-trash-o fa-lg"></i> Delete</a>
+              </span>
+            </h5>
+            <p
+              class="card-text"
+            >This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    post: Object
+  },
+  methods: {
+      edit(){
+          return '/post/edit/' + this.post.id
+      }
+  },
+};
+</script>
+
+<style>
+.post {
+  margin: 20px;
+  -webkit-box-shadow: 10px 10px 5px 0px rgba(214, 214, 214, 1);
+  -moz-box-shadow: 10px 10px 5px 0px rgba(214, 214, 214, 1);
+  box-shadow: 10px 10px 5px 0px rgba(214, 214, 214, 1);
+}
+
+.card.mb-3 {
+  padding: 10px;
+}
+
+.buttons-right {
+  position: absolute;
+  top: 8px;
+  right: 16px;
+}
+</style>
