df145793cb7971714c8a4a2d162c490060d4c773
Matheus Melo Antiquera <matheus.melo.a@hotmail.com>
2019-05-30 21:55:23 -0300

    Atualização das Dependencias

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -12,19 +12,21 @@
             <div class="form-group">
                 <label for="title">Title</label>
                 <input type="text" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}" name="title" id="title" placeholder="Enter post title"
-                    value="@isset($post->title) {{ $post->title }} @endisset"> @if($errors->has('title'))
-                <div class="invalid-feedback">
-                    {{ $errors->first('title') }}
-                </div>
+                    value="@isset($post->title) {{ $post->title }} @endisset">
+                @if($errors->has('title'))
+                    <div class="invalid-feedback">
+                        {{ $errors->first('title') }}
+                    </div>
                 @endif
             </div>
             <div class="form-group">
                 <label for="author">Author</label>
                 <input type="text" class="form-control {{ $errors->has('author') ? 'is-invalid' : '' }}" name="author" id="author" placeholder="Enter your name"
-                    value="@isset($post->author) {{ $post->author }} @endisset"> @if($errors->has('author'))
-                <div class="invalid-feedback">
-                    {{ $errors->first('author') }}
-                </div>
+                    value="@isset($post->author) {{ $post->author }} @endisset">
+                 @if($errors->has('author'))
+                    <div class="invalid-feedback">
+                        {{ $errors->first('author') }}
+                    </div>
                 @endif
             </div>
 
@@ -32,9 +34,9 @@
                 <label for="text">Text</label>
                 <textarea class="form-control {{ $errors->has('text') ? 'is-invalid' : '' }}" name="text" id="text" rows="3">@isset($post->text) {{ $post->text }} @endisset</textarea>
                 @if($errors->has('text'))
-                <div class="invalid-feedback">
-                    {{ $errors->first('text') }}
-                </div>
+                    <div class="invalid-feedback">
+                        {{ $errors->first('text') }}
+                    </div>
                 @endif
             </div>
             <button type="submit" class="btn btn-primary">Save</button>
